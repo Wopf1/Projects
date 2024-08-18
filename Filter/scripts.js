9ae3e334117960c8a481b1ccf017ad5d792e69d4
Wopf1 <musildenis@gmail.com>
2024-08-18 22:08:39 +0200

    before filter

--- Filter/scripts.js
+++ Filter/scripts.js
@@ -1,13 +1,33 @@
-const API = `https://hp-api.onrender.com/api/characters`
-const inputCharName = document.querySelector('.character-name');
-const characterSection = document.querySelector('.characters');
+const API = `https://hp-api.onrender.com/api/characters`;
+const inputCharName = document.querySelector(".character-name");
+const characterSection = document.querySelector(".characters");
 
-const getChar = async (url) => {
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data);
-    
-
+const charImg = (name, image, where) => {
+    const div = document.createElement("div")
+    div.classList.add('character-box')
+    const img = document.createElement("img")
+    div.append(img)
+    img.src = image
+    const p =  document.createElement("p")
+    p.textContent = name
+    div.append(p)
+    where.append(div)    /* return div*/
 }
 
-getChar(API)
+const renderChar = (char) => {
+  char.forEach((element) => {
+    if (element.image) {
+      charImg(element.name ,element.image, characterSection)
+    }
+  });
+};
+
+const getChar = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  renderChar(data)
+};
+
+getChar(API);
+
+
